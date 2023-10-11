55f4c20155afb0b52a4be45352acdf96096c6801
vitmansano <107126458+vitmansano@users.noreply.github.com>
2023-10-10 21:17:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Altera o texto do título
+document.querySelector("h1").textContent = "Programação e Sistemas deInformação";
+// Adiciona um link para o GitHub
+document.querySelector("footer").innerHTML += `
+<a
+href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a
+>`;
